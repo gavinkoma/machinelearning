cdd4b085fcd8c8e416e1ff4475a9399715155189
Gavin Koma <gavintkoma@gmail.com>
2022-09-26 22:38:14 -0400

    updated

--- homework2/kNNprac.py
+++ homework2/kNNprac.py
@@ -4,28 +4,35 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
 import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from sklearn import neighbors, datasets
-
-#import the data first
-iris = datasets.load_iris()
+cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
 
-#pseudocode 
-# predictor = create(algorithm_type, D)
-# y_new = predictor(x_new)
-
-#why dont we start by initially using the first two features
-#and we can define our k value
-k = 10
-x = iris.data[:,[0,1]]
-y = iris.target
+#our sample is classified by the weight of the nearest neighbors
+#lets do two classes to start and a feature vector
+#how any nearest neighbors do we want to look at? 3? 4?
+#remember that were using euclidean distance
 
 
-print(iris.target)
+
+
+class kNN:
+    
+    def __init__(self, k=3):
+        self.k=k
+        
+    #so we now have our class
+    
+    def fit(self,X,y): #will probably be our training step
+        pass
+    
+    def predict(self,X):
+        pass
+    
 
 
 
