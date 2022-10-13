52ac67665af93b5444b573f48d0e1655d5cc30b6
Gavin Koma <gavintkoma@gmail.com>
2022-10-13 13:43:39 -0400

    added question 2

--- homework4/trial1.py
+++ homework4/trial1.py
@@ -5,8 +5,6 @@ Created on Thu Oct 13 11:47:31 2022
 
 @author: gavinkoma
 """
-
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,7 +18,7 @@ import matplotlib.pyplot as plt
 #f_xx = 10*x_1 + 8*x_2 - 5
 
 
-## question 2
+#%% ## question 2
 #You are given a function f(x) = sin(x) +0.3x
 x_val = [*range(0,100)]
 x = np.arange(-10,10,0.1)
@@ -30,11 +28,35 @@ plt.show()
 
 #derive a gradient descent iteration formula for finding a (local) minimum of f(x)
 #we have to repeat our functions until we find convergence
-x_dev = np.cos(x) + 0.3
-y_dev = np.arange(-10,10,0.1)
+y_dev = np.cos(x) + 0.3
+x_dev = np.arange(-10,10,0.1)
 plt.plot(x_dev,y_dev)
 plt.show()
 
-def mean_squared_error
+cur_x = [-0] #algorithm will start at 3
+rate = 0.01 #learning rate
+alpha = rate
+max_iters = 1000 #maximum number of iterations
+
+def gradient(val,alpha,max_iters):
+    
+    precision = 0.000001 #lets stop the algorithm here
+    previous_step_size = 1
+    
+    iters = 0
+    df = lambda x: np.cos(x)+0.3 #gradient
+    
+    while previous_step_size > precision and iters < max_iters:
+        prev_x = val #store the current x value in prev_x
+        val = val-rate*df(prev_x) #grad descent
+        previous_step_size = abs(val - prev_x)
+        iters = iters+1
+        print("Iteration",iters,"\nX value is",val)
+    
+    return
+
+for val in cur_x:
+    gradient(val, alpha, max_iters)
+
 
 
