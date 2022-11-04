21fb1c615a5dc98088c80a1e44192ccf8c452e98
Gavin Koma <gavintkoma@gmail.com>
2022-11-04 09:17:49 -0700

    my pictures of 2.7 arent loading but im getting values

--- homework5/allvals.py
+++ homework5/allvals.py
@@ -6,97 +6,102 @@ Created on Thu Nov  3 15:09:40 2022
 @author: gavinkoma
 """
 #start with the proper libraries i guess
-import numpy as np
+from tensorflow.keras.utils import to_categorical
+from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
-from keras.utils import np_utils
-from tensorflow.keras.optimizers import Adam
-from keras.datasets import mnist
-from sklearn.model_selection import KFold
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
+#from step 0
+nb_epoch = 5
+batch_size = 64
+nb_filters = 32
+nb_pool = 2
+nb_conv = 3
 
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+img_rows, img_cols = 28, 28
+shape_ord = (img_rows,img_cols,1)
+
+#normalization
+X_train = X_train[:20000]
+Y_train = y_train[:20000]
+X_train = X_train.reshape((X_train.shape[0],) + shape_ord)
+X_test = X_test.reshape((X_test.shape[0],) + shape_ord)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train/255
+X_test = X_test/255
+Y_train = to_categorical(Y_train)
+Y_test = to_categorical(y_test)
+
+nb_classes = 10
+
 X_test_orig = X_test
 
-#recall that we want all 10 digits
-#lets convert y_train and y_test to categorical 
+# Function for constructing the convolution neural network
+# Feel free to add parameters, if you want
 
-X_train = X_train.reshape((X_train.shape[0],28,28,1))
-X_test = X_test.reshape((X_test.shape[0],28,28,1))
+def build_model(num_conv = 1, conv_activation = "relu", num_dense = 1, dense_activation  = "relu", 
+               dropout = True, max_pooling = True):
+    """"""
+    model = Sequential()
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv), 
+                     padding='valid',
+                     input_shape=shape_ord))
+    model.add(Activation(conv_activation))
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv)))
+    model.add(Activation(conv_activation)) 
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(128))
+    model.add(Activation(dense_activation))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+    model.fit(X_train, Y_train, batch_size=batch_size, 
+              epochs=nb_epoch,verbose=1,
+              validation_data=(X_test, Y_test))
+          
 
-#convert to categorical
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-
-#now we need to normalize the pixel values
-train_norm = X_train.astype('float32')
-test_norm = X_test.astype('float32')
-
-train_norm = train_norm/255.0
-test_norm = test_norm/255.0
-
-
-nb_epoch = 5  # kept very low! Please increase if you have GPU
-
-batch_size = 128
-# number of convolutional filters to use
-nb_filters = 32
-# size of pooling area for max pooling
-nb_pool = 2
-# convolution kernel size
-nb_conv = 3
-
-#we previously designated 7 as 1 and everything else as 0
-#via binary classification, but we dont need to do this
-#because were using all the numbers, so Im just going to skip it
-
-model = Sequential()
-model.add(Conv2D(32, (3,3), activation = 'relu',kernel_initializer='he_uniform',input_shape=(28,28,1)))
-model.add(MaxPooling2D((2,2)))
-model.add(Flatten())
-model.add(Dense(100,activation='relu',kernel_initializer='he_uniform'))
-model.add(Dense(10,activation='softmax'))
-
-#compile it
-opt = Adam(learning_rate=0.0002, amsgrad=True)
-model.compile(optimizer=opt,loss='categorical_crossentropy',metrics=['accuracy'])
-
-#i guess we can use cross validation but?
-#im not entirely sure? it would work to keep the eval times low
-#since our data sets wont be quite small 
-
-scores, histories = [],[] 
-
-
-kfold = KFold(5,shuffle=True,random_state=1)
-#enumerate baybeeeee
-
-model.summary()
-
-# Evaluating the model on the test data    
-loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
-print('Test Loss:', loss)
-print('Test Accuracy:', accuracy)
+    #Evaluating the model on the test data    
+    score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+    print(num_conv , 'convolutional layers,', num_dense, "dense layers")
+    if max_pooling: print("With max pooling")
+    if dropout: print("With dropout")
+    print('Test score:', score)
+    print('Test accuracy:', accuracy)
+    print(model.summary())
+    show_results(model)
+    #return accaccuracy
 
 def show_results(model):
     rows = 4
     columns = 15
     sliced = rows*columns
     predicted = model.predict(X_test[:sliced]).argmax(-1)
-
     plt.figure(figsize=(16,8))
     for i in range(sliced):
         plt.subplot(rows, columns, i+1)
         plt.imshow(X_test_orig[i], cmap='gray', vmin=0, vmax=255)
-        color = 'black' if y_test[i, 1] == predicted[i] else 'red'
+        color = 'black' if np.where(Y_test[i] == 1)[0][0] == predicted[i] else 'red'
         plt.text(0, 0, predicted[i], color=color, 
                  bbox=dict(facecolor='white', alpha=1))
         plt.axis('off')
 
-show_results(model)
+
+build_model(num_conv = 3, num_dense = 2)
+
+
+
+
 
 
--- homework5/cifar.py
+++ homework5/cifar.py
@@ -6,52 +6,10 @@ Created on Thu Nov  3 18:00:06 2022
 @author: gavinkoma
 """
 
-import keras
-from keras.datasets import cifar10
-from keras.models import Sequential
-from keras import datasets, layers, models
-from keras.utils import np_utils
-from keras import regularizers
-from keras.layers import Dense, Dropout, BatchNormalization
-import matplotlib.pyplot as plt
-import numpy as np
+from keras import backend as K
+from tensorflow.keras.utils import to_categorical
+from keras.datasets import mnist
 
-
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
-# Creating a list of all the class labels
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
-
-# Visualizing some of the images from the training dataset
-plt.figure(figsize=[10,10])
-for i in range (25):    # for first 25 images
-  plt.subplot(5, 5, i+1)
-  plt.xticks([])
-  plt.yticks([])
-  plt.grid(False)
-  plt.imshow(train_images[i], cmap=plt.cm.binary)
-  plt.xlabel(class_names[train_labels[i][0]])
-
-plt.show()
-
-train_images=train_images.astype('float32')
-test_images=test_images.astype('float32')
-
-#standardize your images
-train_images = train_images/255
-test_images = test_images/255
-
-#one hot encoding for our target class
-num_classes = 10
-train_labels = np_utils.to_categorical(train_labels,num_classes)
-
-
-model = Sequential()
-
-#im not sure how many layers of this we actually want 
-#so we can try to make one and test out the accuracy and then just
-#progress from there? 
 
 
 
