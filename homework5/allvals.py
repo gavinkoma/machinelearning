9be2b4cee4839c0c631acad7eb7f371c0080c072
Gavin Koma <gavintkoma@gmail.com>
2022-11-04 08:29:49 -0700

    cifar

--- homework5/allvals.py
+++ homework5/allvals.py
@@ -39,6 +39,17 @@ test_norm = X_test.astype('float32')
 train_norm = train_norm/255.0
 test_norm = test_norm/255.0
 
+
+nb_epoch = 5  # kept very low! Please increase if you have GPU
+
+batch_size = 128
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+
 #we previously designated 7 as 1 and everything else as 0
 #via binary classification, but we dont need to do this
 #because were using all the numbers, so Im just going to skip it
@@ -66,9 +77,26 @@ kfold = KFold(5,shuffle=True,random_state=1)
 
 model.summary()
 
+# Evaluating the model on the test data    
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print('Test Loss:', loss)
+print('Test Accuracy:', accuracy)
 
+def show_results(model):
+    rows = 4
+    columns = 15
+    sliced = rows*columns
+    predicted = model.predict(X_test[:sliced]).argmax(-1)
 
+    plt.figure(figsize=(16,8))
+    for i in range(sliced):
+        plt.subplot(rows, columns, i+1)
+        plt.imshow(X_test_orig[i], cmap='gray', vmin=0, vmax=255)
+        color = 'black' if y_test[i, 1] == predicted[i] else 'red'
+        plt.text(0, 0, predicted[i], color=color, 
+                 bbox=dict(facecolor='white', alpha=1))
+        plt.axis('off')
 
-
+show_results(model)
 
 
