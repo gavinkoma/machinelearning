9e7e53eac1ffb9a8d9f7c30ad83a5f81aad536d9
Gavin Koma <gavintkoma@gmail.com>
2022-11-17 16:09:05 -0500

    we have an rnn but it doesnt work ideally, have some output, might want to train a bit more with gpu

--- final_project/trial1.py
+++ final_project/trial1.py
@@ -12,12 +12,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from tensorflow.keras.optimizers import RMSprop
-from keras.utils.data_utils import get_file
 import numpy as np
 import random
 import re
 import sys
-import io
 import pandas as pd
 
 #okay start by downloading data and reading through it
@@ -80,5 +78,100 @@ with open('tweet.txt','w') as f:
     f.write(cleantweets_string)
 
 #okay so now we should start modeling the RNN
+#we want to look at the values in cleentweets string
+
+chars = sorted(list(set(cleantweets_string)))
+print('total chars:', len(chars)) #191 chars
+char_indices = dict((c,i) for i,c in enumerate(chars))
+indices_char = dict((i,c) for i,c in enumerate(chars))
+
+
+maxlen = 100
+step = 5
+sentences = []
+next_chars = []
+
+for i in range(0, len(cleantweets_string) - maxlen, step):
+    sentences.append(cleantweets_string[i: i + maxlen])
+    next_chars.append(cleantweets_string[i + maxlen])
+print('nb sequences:', len(sentences))
+
+print('Vectorization...')
+x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+print('Done!')
+
+# build the model: a single LSTM
+print('Build model...')
+model = Sequential()
+model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+optimizer = RMSprop(learning_rate=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+print('Done!')
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.ma.log(preds)
+    preds = preds.filled(0)
+    preds = preds / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+def on_epoch_end(epoch, logs):
+    # clear_output()
+    # Function invoked at end of each epoch. Prints generated text.
+    print()
+    print('----- Generating text after Epoch: %d' % (epoch+1))
+
+    start_index = random.randint(0, len(cleantweets_string) - maxlen - 1)
+    for diversity in [0.2, 0.4, 0.5, 1.0]:
+        print('----- diversity:', diversity)
+
+        generated = ''
+        sentence = cleantweets_string[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(400):
+            x_pred = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x_pred[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
+
+print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+
+epoch_val = [*range(1,10)]
+
+model.fit(x, y,
+          batch_size=128,
+          epochs=1,
+          callbacks=[print_callback])
+
+
+
+
+
 
 
